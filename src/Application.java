7b37faa010aa88f16c622963569443b43928c966
somename <someemail>
2016-09-21 14:51:12 +0300

    besouro automatic message

--- src/Application.java
+++ src/Application.java
@@ -8,15 +8,15 @@ public class Application {
 		BowlingGame game = new BowlingGame();
 		Frame[] frame = new Frame[10];
 		frame[0] = new Frame(1,5);
-		frame[1] = new Frame(1,5);
-		frame[2] = new Frame(1,5);
-		frame[3] = new Frame(1,5);
-		frame[4] = new Frame(1,5);
-		frame[5] = new Frame(1,5);
-		frame[6] = new Frame(1,5);
-		frame[7] = new Frame(1,5);
-		frame[8] = new Frame(1,5);
-		frame[9] = new Frame(1,5);
+		frame[1] = new Frame(3,6);
+		frame[2] = new Frame(7,2);
+		frame[3] = new Frame(3,6);
+		frame[4] = new Frame(4,4);
+		frame[5] = new Frame(5,3);
+		frame[6] = new Frame(3,3);
+		frame[7] = new Frame(4,5);
+		frame[8] = new Frame(8,1);
+		frame[9] = new Frame(2,6);
 		
 	}
 
