7fed990be2ad933c98c55ba868f69b114d3821b6
somename <someemail>
2016-09-21 14:59:26 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -19,7 +19,7 @@ public class Frame {
 	public int score(){
 		int score = 0;
 		if(this.isStrike()) {
-			score = 10 ;
+			score = 10;
 		} else {
 			score = this.firstThrow+this.secondThrow;
 		}
