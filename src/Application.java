44bf7193b550630b386277860aa26556ab0059dd
somename <someemail>
2016-09-21 14:52:45 +0300

    besouro automatic message

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,7 @@
 public class Application {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BowlingException {
 		
 		
 		BowlingGame game = new BowlingGame();
--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -29,6 +29,9 @@ public class BowlingGame {
 	public int score(){
 		int gameScore = 0;
 		for(Frame frame : this.frames) {
+			if(frame.isStrike()) {
+				
+			}
 			gameScore = gameScore + frame.score();
 		}
 		return gameScore;
