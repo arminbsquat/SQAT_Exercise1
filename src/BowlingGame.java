a951b54496e3caa1a43d4f837b42f5036bee45e7
somename <someemail>
2016-09-21 15:34:41 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -37,7 +37,7 @@ public class BowlingGame {
 			if(frames.get(i).isStrike() && i+1 < frames.size()) {
 				frames.get(i).updateScore(frames.get(i+1).score);
 			}
-			if(frames.get(i).isSpare()) {
+			if(frames.get(i).isSpare() && i+1 < frames.size()) {
 				frames.get(i).updateScore(frames.get(i+1).getFirstThrow());
 			}
 		}
