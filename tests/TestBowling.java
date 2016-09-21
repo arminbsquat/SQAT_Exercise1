8e93b87fadb209ab8709efe65d179d6b5026b3ca
somename <someemail>
2016-09-21 16:10:03 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -8,7 +8,22 @@ public class TestBowling {
 	
 	@Test
 	public void testNoStrikesNoSpares() {
+		BowlingGame game = new BowlingGame();
+		Frame[] frame = new Frame[10];
+		frame[0] = new Frame(1,5);
+		frame[1] = new Frame(3,6);
+		frame[2] = new Frame(7,2);
+		frame[3] = new Frame(3,6);
+		frame[4] = new Frame(4,4);
+		frame[5] = new Frame(5,3);
+		frame[6] = new Frame(3,3);
+		frame[7] = new Frame(4,5);
+		frame[8] = new Frame(8,1);
+		frame[9] = new Frame(2,6);
 		
+		for( int i = 0; i < 10; i++) {
+			game.addFrame(frame[i]);
+		}
 	}
 	
 	
