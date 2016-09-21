6ebd37ff581d2c67764abcf87439d591fa4bb7b6
somename <someemail>
2016-09-21 14:41:45 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -14,7 +14,7 @@ public class BowlingGame {
 	// adds a frame to the game
 	public void addFrame(Frame frame){
 		if(this.frames.size() > 10) {
-			throw new BowlingExceptio();
+			throw new BowlingException();
 		}
 		this.frames.add(frame);
 		
