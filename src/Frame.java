068f27b365ed1228e1f9e310b21c829166b4927f
somename <someemail>
2016-09-21 15:14:03 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -1,8 +1,7 @@
 public class Frame {
 	private int firstThrow;
 	private int secondThrow;
-	public int frameNumber;
-	public static int globalFrameNumber = 0;
+	
 	
 	public Frame(int firstThrow, int secondThrow){
 		this.firstThrow = firstThrow;
@@ -30,10 +29,8 @@ public class Frame {
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
 		if(this.getFirstThrow() == 10) {
-			this.strike = true;
 			return true;
 		} else {
-			this.strike = false;
 			return false;
 		}
 	}
