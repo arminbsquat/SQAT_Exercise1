4827aec564044ec753f503775ba081926908b000
somename <someemail>
2016-09-21 16:15:19 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -27,7 +27,25 @@ public class TestBowling {
 		assertEquals(game.score(), 81);
 	}
 	
-	
+	public void testOneSpare() {
+		BowlingGame game = new BowlingGame();
+		Frame[] frame = new Frame[10];
+		frame[0] = new Frame(1,5);
+		frame[1] = new Frame(3,6);
+		frame[2] = new Frame(7,2);
+		frame[3] = new Frame(3,6);
+		frame[4] = new Frame(4,4);
+		frame[5] = new Frame(5,3);
+		frame[6] = new Frame(3,3);
+		frame[7] = new Frame(4,5);
+		frame[8] = new Frame(8,1);
+		frame[9] = new Frame(2,6);
+		
+		for( int i = 0; i < 10; i++) {
+			game.addFrame(frame[i]);
+		}
+		assertEquals(game.score(), 81);
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
