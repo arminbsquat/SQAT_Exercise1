5055262557b2952c1149405b54d0cc558476b372
somename <someemail>
2016-09-21 15:15:59 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -2,6 +2,7 @@ public class Frame {
 	private int firstThrow;
 	private int secondThrow;
 	boolean isStrike;
+	int myScore;
 	
 	public Frame(int firstThrow, int secondThrow){
 		this.firstThrow = firstThrow;
